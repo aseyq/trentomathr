6943c066db821071574166b8ef089f44c2cfe1f9
Ali Seyhun Saral <82848414+aseyq@users.noreply.github.com>
2021-11-08 14:06:06 +0100

    Create excercise1.R

---
+++ excercise1.R
@@ -0,0 +1,34 @@
+## Excercise 1
+
+# (Note that if a line starts with #, R will ignore the line)
+# So we can add comments on the code
+
+# Question 1
+# Calculate the multiplication of 4, 8 and 12
+
+4 *
+
+# Question 2    
+# - Create a variable named `height` with value 4, and another called `width` 10
+# - Calculate the area of the rectangle and asign the value to the variable area
+# - Check if the value <50
+# - Check if the value is equal to 40    
+    
+height <- ...
+... <- ...
+... <- ...
+
+... < 50
+... ..  10
+
+
+# Question 3
+# Calculate the distance between the points -4 and 2 using the absolute value (abs) function
+
+
+# Question 4
+# We have two points (x1,y1) and (x2,y2) in a cartesian system.
+# Create a function that calculates their distance and call it distance
+# Caluclate the distance of (0,0) and (3,4)
+# Calculate the distance of (3,3) and (-1,4)
+
