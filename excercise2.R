f742e5e7cd32f0ad5c80389ba7e98781a5e60487
aseyq <aseyq@posteo.net>
2021-11-15 16:53:42 +0100

    week2

---
+++ excercise2.R
@@ -0,0 +1,21 @@
+## Excercise 2
+
+# 1- Create a variable called `my_numeric` with the number 320. 
+# Check the type of the variable. Then convert it to character and save the result as `my_character`.
+
+
+# 2- Calculate the sum and subtraction of the vectors (4,1) and (3,5). Then multipy these vectors. What did you get? Is that a proper vector multiplication?
+
+
+# 3- Here are the temperature of Trento in the previous years
+# 2021:  50.2 Fahrenheit  
+# 2020:  42.4 Fahrenheit  
+# 2019:  34.4 Fahrenheit  
+# 2018:  46.3 Fahrenheit  
+# 2017:  41.24 Fahrenheit  
+
+# a- Create a function which convert Fahrenheit to Celsius. Call it `f_to_c`
+# b- Convert each of these values to Celsius
+# c- Create a vector from the years and save it as `vector_year`
+# d- Create a vector from the celsius values and save it as `vector_celsius`
+
