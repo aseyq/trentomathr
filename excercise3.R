450cb22fb1d11f74151d6a8f61bcb2c3e4427fca
aseyq <aseyq@posteo.net>
2021-11-22 14:20:38 +0100

    add slides 3 and excercise 3

---
+++ excercise3.R
@@ -0,0 +1,31 @@
+# Excercises 3
+
+# 1 - Put the data of the guesses in a vector. (if you can read from csv file... good. but a simple copy past will suffice)
+
+guesses <- c(
+    10, #
+    20, # Delete this lines and add the real values
+    30, # 
+    ... #
+)
+
+
+# 2 - Calculate the mean and save it to `mean_guesses`
+
+mean_guesses
+
+
+# 3- Draw a histogram from the guesses data
+
+# 4 - Put the real number as a vertical line on the plot.
+
+# 5 - Think about the results. Do you think it was accurate? Were you expecting it? How well did you do?
+
+# 6 - Create the vector of deviations (differences) from the real number. Save it as `accuracies`
+
+# 7 - Now create a vector from the seating row data. save it as `seatings`
+
+# 8 - Plot a graph with `seatings` on the x axis and `accuracies` on the y axis.
+
+# 9 - Interpret the graph. Do you think the people on the front rows did better?
+
